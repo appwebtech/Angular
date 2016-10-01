ff183208d0a2838a50184f87d54de0c14017aec5
appwebtech <josembi@gmail.com>
2016-10-01 21:14:00 +0200

    AngularJS ng include Directive

---
+++ Angular_1/16_AngularJS ng include Directive/scripts/script3.js
@@ -0,0 +1,15 @@
+var app = angular
+        .module("myModule", [])
+        .controller("myController", function ($scope) {
+          var employees = [
+            { name: "Joe", gender: "Male", salary: "65000" },
+            { name: "Jude", gender: "Male", salary: "55000" },
+            { name: "Jody", gender: "Male", salary: "25000" },
+            { name: "Judy", gender: "Female", salary: "45000" },
+            { name: "Jane", gender: "Female", salary: "35000" },
+            { name: "Jobs", gender: "Male", salary: "85000" }
+          ];
+
+            $scope.employees = employees;
+            $scope.employeeView = "EmployeeList.html";
+        });
