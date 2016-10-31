47b043dc74251d8af0b1a50127bd448458b906c1
appwebtech <josembi@gmail.com>
2016-10-31 06:30:42 +0100

    .

---
+++ Angular_1/Unprofessional_Instructables/28_$scope.$watch()/controller2.js
@@ -0,0 +1,11 @@
+
+var app = angular.module('mainApp', []);
+
+app.controller('app', function($scope){
+	$scope.counter = 0;
+	$scope.$watch('kiatuShoe', function(newValue, oldValue){
+			// console.log("New Value:" + newValue); 
+			// console.log("Old Value:" + oldValue);
+			$scope.counter++
+	})
+});
