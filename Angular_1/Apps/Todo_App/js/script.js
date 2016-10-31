e388e8d5f9f39b721a43e2b6db914504edead20e
appwebtech <josembi@gmail.com>
2016-10-31 11:37:21 +0100

    Add more Functionalitites

--- Angular_1/Apps/Todo_App/js/script.js
+++ Angular_1/Apps/Todo_App/js/script.js
@@ -1,3 +1,19 @@
 
-var application = angular.module('joSembi', []);
+var appWebTech = angular.module('joSembi', []);
 
+
+appWebTech.controller('app', function($scope){
+
+	$scope.tasks = [];
+	$scope.searchEnter = function(){
+		if(event.which == 13 && $scope.task != ""){
+			$scope.addTask();
+		}
+	};
+	$scope.addTask = function(){   // Will transfer text from textbox to browser.
+
+			$scope.tasks.push($scope.task);
+			console.log($scope.task);
+			$scope.task = "";
+	};
+});
